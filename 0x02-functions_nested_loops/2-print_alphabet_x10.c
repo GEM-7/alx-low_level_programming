a212360fb0108af2bf475d29e08fd05fead9dc1a
GEM7 <gohemmanuel7@gmail.com>
2023-09-12 17:00:24 -0700

    ${var}

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabets 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y = 0;
+
+	while (y < 10)
+	{
+		for (x = 'a'; x <= 'z'; a++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
