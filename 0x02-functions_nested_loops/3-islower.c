854635ecc96e1e7f11b8b8e8367db8a34bb3e01a
GEM7 <gohemmanuel7@gmail.com>
2023-09-13 21:19:25 -0700

    ${var}

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: parameter to be used
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
