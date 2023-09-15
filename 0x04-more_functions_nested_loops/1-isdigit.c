ae8e1b019cd0aa38d1fb0c9987416152c209a755
GEM7 <gohemmanuel7@gmail.com>
2023-09-15 01:23:02 -0700

    Write a function that checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * _isdigit - function that checks for a digit (0 through 9)
- *   * @c: parameter to be checked
- *    * Return: 1 if c is a digit
- *     * otherwise 0
- *      */
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: parameter to be checked
+ * Return: 1 if c is a digit
+ * otherwise 0
+ */
 int _isdigit(int c)
 {
 		if ((c >= 48) && (c <= 57))
