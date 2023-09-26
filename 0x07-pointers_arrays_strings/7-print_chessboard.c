d8496dd15bb2b35dacb939f5dd32a05dce31b90c
GEM7 <gohemmanuel7@gmail.com>
2023-09-25 22:50:45 -0700

    Update main.h

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: The 2D array representing the chessboard.
+ *
+ * Return: None.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			putchar(a[row][col]);
+		}
+		putchar('\n');
+	}
+}
