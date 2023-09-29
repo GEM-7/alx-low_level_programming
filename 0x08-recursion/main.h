b1bc3c1afaef1ffc81c533bb61db4a09dffc04cf
GEM7 <gohemmanuel7@gmail.com>
2023-09-28 22:24:53 -0700

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
-#ifndef "main_h"
-#define "main_h"
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
